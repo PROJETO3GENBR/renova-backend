7caad08c4e6c34d0655eff8c77f82ec855666c94
levylima <levylima1510@gmail.com>
2022-10-13 11:00:45 -0300

    Criando o projeto com Nest

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'prjintegrador',
+      entities: [],
+      synchronize: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}

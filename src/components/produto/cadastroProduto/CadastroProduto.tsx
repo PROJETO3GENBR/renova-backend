8d6ca318f9c7871cd6b62b5bcc68fdfb59f6de59
LunaDuram <lucineiaduram@gmail.com>
2022-12-08 15:02:51 -0300

    atualização

--- src/components/produto/cadastroProduto/CadastroProduto.tsx
+++ src/components/produto/cadastroProduto/CadastroProduto.tsx
@@ -10,17 +10,17 @@ function CadastroProduto() {
     return (
         <Container maxWidth="sm" className="topo">
             <form >
-                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro postagem</Typography>
+                <Typography variant="h3" color="textSecondary" component="h1" align="center" >Formulário de cadastro produto</Typography>
                 <TextField id="titulo" label="titulo" variant="outlined" name="titulo" margin="normal" fullWidth />
                 <TextField id="texto" label="texto" name="texto" variant="outlined" margin="normal" fullWidth />
 
                 <FormControl >
-                    <InputLabel id="demo-simple-select-helper-label">Tema </InputLabel>
+                    <InputLabel id="demo-simple-select-helper-label">Categoria </InputLabel>
                     <Select
                         labelId="demo-simple-select-helper-label"
                         id="demo-simple-select-helper">
                     </Select>
-                    <FormHelperText>Escolha um tema para a postagem</FormHelperText>
+                    <FormHelperText>Escolha uma categoria para o produto</FormHelperText>
                     <Button type="submit" variant="contained" color="primary">
                         Finalizar
                     </Button>

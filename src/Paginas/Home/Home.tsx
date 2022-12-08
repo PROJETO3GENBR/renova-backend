da58a66c88968ab6f259e60df1f549149a857ae6
LunaDuram <lucineiaduram@gmail.com>
2022-12-08 14:50:51 -0300

    Home

--- src/Paginas/Home/Home.tsx
+++ src/Paginas/Home/Home.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import Navbar from "../../components/estaticos/Navbar/Navbar";
+import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
+import TabProduto from "../../components/produto/tabprodutos/TabProduto";
 
-function Home () {
+function Home() {
     return (
         <>
-        <h1>Home</h1>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className='botao'>Ver Produto</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} className='produtos'>
+                <TabProduto/>
+                </Grid>
+            </Grid>
         </>
-    )
+    );
 }
 
-export { Home };
+export {Home};

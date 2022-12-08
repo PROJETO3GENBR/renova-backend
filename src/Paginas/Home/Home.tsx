10fa5d290ee1bf9b3e7f3e1f9990d259fd51086a
LunaDuram <lucineiaduram@gmail.com>
2022-12-08 15:37:12 -0300

    atualização

--- src/Paginas/Home/Home.tsx
+++ src/Paginas/Home/Home.tsx
@@ -12,14 +12,6 @@ function Home() {
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
                         <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                        </Box>
-                        <Button variant="outlined" className='botao'>Ver Produto</Button>
-                    </Box>
-                </Grid>
-                <Grid item xs={6} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className='produtos'>
                 <TabProduto/>

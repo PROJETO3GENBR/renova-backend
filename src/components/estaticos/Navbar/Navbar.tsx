f389842a95cb4e000c0b8dbbb068d5c8d2e1b99f
levylima <levylima1510@gmail.com>
2022-12-21 19:03:00 -0300

    navbar optimizations

--- src/components/estaticos/Navbar/Navbar.tsx
+++ src/components/estaticos/Navbar/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
 
   let button;
   if (token != '') {
-    button = <Button id='botao_nav' onClick={logout} >LOGOUT </Button>
+    button = <Button id='botao_nav' onClick={logout} >Sair </Button>
   } else {
     button = <Button id='botao_nav' href='/login'>Login</Button>
   }
